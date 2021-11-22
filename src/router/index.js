55639f13a20905330e32ca9a97bc46c5961e8705
Libamlak Birhanu <realweirdo904@gmail.com>
2021-11-22 14:02:49 +0300

    implemented vue router guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
 import Home from '../views/Home.vue';
 import NotFound from '../views/NotFound.vue';
+// import store from './../store';
 
 const routes = [
 	{
-		path: '/',
+		path: '/login',
 		name: 'Login',
 		component: Login,
 	},
@@ -16,9 +17,10 @@ const routes = [
 		component: Register,
 	},
 	{
-		path: '/home',
+		path: '/',
 		name: 'Home',
 		component: Home,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/:catchAll(.*)',
@@ -29,6 +31,18 @@ const routes = [
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.requiresAuth) {
+		if (!store.state.isAuthenticated) {
+			next({
+				name: 'Login',
+			});
+		}
+	} else {
+		next();
+	}
 });
 
 export default router;
